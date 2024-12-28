fb6d76c60f8d45f74422363b5232291a00163caa
Ziya Hacili <mziyahajl@mail.ru>
2024-12-29 01:18:15 +0400

    add yaml files for kubernetes

--- Soil/obj/Debug/net8.0/Soil.AssemblyInfo.cs
+++ Soil/obj/Debug/net8.0/Soil.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Soil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04c4b70f76c88441fb90698e498de835a908a19d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e53d2a6e85e4c7bebb7b034eeb3e083eafe5bc4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Soil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Soil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
